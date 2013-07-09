2667e33038b5068dec0cd329d2d9809a88a391c7
absalon-james <james.absalon@rackspace.com>
2013-07-09 20:49:23 +0000

    Moving towards a package as described in hitch hiker's guide to packaging.  Still not handling prerequisites

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="Velopyraptor",
+    version="0.2dev",
+    packages=['velopyraptor',],
+    license='Apache License, Version 2.0',
+    long_description=open('README.txt').read()
+)
