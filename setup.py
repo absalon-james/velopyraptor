2a6cae81c10e699621098b2aa822ae55865ff200
absalon-james <james.absalon@rackspace.com>
2013-07-10 16:06:10 +0000

    checking in changes to setup

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 setup(
     name="Velopyraptor",
     version="0.2dev",
-    packages=['velopyraptor',],
+    packages=['velopyraptor', 'velopyraptor.distributions',],
     license='Apache License, Version 2.0',
+    requires=['bitarray', 'numpy',],
     long_description=open('README.txt').read()
 )
