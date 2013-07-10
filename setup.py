315b732d7f523c9ec0a0c15c0ca27135dcd78b12
absalon-james <james.absalon@rackspace.com>
2013-07-10 16:13:09 +0000

    Changing requires to install-requires

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="Velopyraptor",
     version="0.2dev",
     packages=['velopyraptor', 'velopyraptor.distributions',],
     license='Apache License, Version 2.0',
-    requires=['bitarray', 'numpy',],
+    install_requires=['bitarray', 'numpy',],
     long_description=open('README.txt').read()
 )
