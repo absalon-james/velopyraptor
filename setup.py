75324b867d630dccbff7025fdb1d4d83811d400a
absalon-james <james.absalon@rackspace.com>
2013-08-13 15:18:47 +0000

    Fixed omission of networkx as a requirement for this package

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.2dev",
     packages=['velopyraptor', 'velopyraptor.distributions',],
     license='Apache License, Version 2.0',
-    install_requires=['bitarray', 'numpy',],
+    install_requires=['bitarray', 'networkx', 'numpy',],
     long_description=open('README.txt').read()
 )
