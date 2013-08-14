d91fb50094b48b5d1b566b106c8e0707c57a1ce7
absalon-james <james.absalon@rackspace.com>
2013-08-14 20:51:55 +0000

    Changed setup.py to not require numpy but still need to add some way of getting boost extension inside

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.2dev",
     packages=['velopyraptor', 'velopyraptor.distributions',],
     license='Apache License, Version 2.0',
-    install_requires=['bitarray', 'networkx', 'numpy',],
+    install_requires=['bitarray', 'networkx', ],
     long_description=open('README.txt').read()
 )
